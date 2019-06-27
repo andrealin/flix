ae91a588d3b6c620902bf8658b6b6ba5389a3c7f
Andrea Lin <drealin@fb.com>
2019-06-26 17:11:58 -0700

    custom cells

---
+++ flix/Views/MovieCell.h
@@ -0,0 +1,20 @@
+//
+//  MovieCell.h
+//  flix
+//
+//  Created by drealin on 6/26/19.
+//  Copyright © 2019 drealin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
