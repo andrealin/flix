5b169c95b393c6477b6d81857ee7c45114dc54e7
Andrea Lin <drealin@fb.com>
2019-06-27 15:17:40 -0700

    segue to details

---
+++ flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  flix
+//
+//  Created by drealin on 6/27/19.
+//  Copyright © 2019 drealin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
