30158f0fa208098b0817953d27895d0842f31a4a
Andrea Lin <drealin@fb.com>
2019-06-28 16:05:57 -0700

    For the large poster, load the low resolution image first and then switch to the high resolution image when complete

--- flix/View Controllers/DetailsViewController.m
+++ flix/View Controllers/DetailsViewController.m
@@ -26,6 +26,7 @@
     
     
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *smallBaseURLString = @"https://image.tmdb.org/t/p/w200";
     NSString *posterURLString = self.movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
@@ -59,36 +60,50 @@
     
     NSString *backdropURLString = self.movie[@"backdrop_path"];
     NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
+    NSString *smallFullBackdropURLString = [smallBaseURLString stringByAppendingString:backdropURLString];
     NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
+    NSURL *smallBackdropURL = [NSURL URLWithString:smallFullBackdropURLString];
 //    [self.backdropView setImageWithURL:backdropURL];
 //
     NSURLRequest *backdropRequest = [NSURLRequest requestWithURL:backdropURL];
-    
+    NSURLRequest *smallBackdropRequest = [NSURLRequest requestWithURL:smallBackdropURL];
+
     //    cell.posterView.image = nil;
     //    [cell.posterView setImageWithURL:posterURL];
     
-    [self.backdropView setImageWithURLRequest:backdropRequest placeholderImage:nil
-                                    success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
-                                        
-                                        // imageResponse will be nil if the image is cached
-                                        if (imageResponse) {
-                                            NSLog(@"Backdrop Image was NOT cached, fade in image");
-                                            self.backdropView.alpha = 0.0;
-                                            self.backdropView.image = image;
-                                            
-                                            //Animate UIImageView back to alpha 1 over 0.3sec
-                                            [UIView animateWithDuration:0.3 animations:^{
-                                                self.backdropView.alpha = 1.0;
-                                            }];
-                                        }
-                                        else {
-                                            NSLog(@"Backdrop Image was cached so just update the image");
-                                            self.backdropView.image = image;
-                                        }
-                                    }
-                                    failure:^(NSURLRequest *request, NSHTTPURLResponse * response, NSError *error) {
-                                        // do something for the failure condition
-                                    }];
+    [self.backdropView setImageWithURLRequest:smallBackdropRequest
+                          placeholderImage:nil
+                                   success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *smallImage) {
+                                       
+                                       // smallImageResponse will be nil if the smallImage is already available
+                                       // in cache (might want to do something smarter in that case).
+                                       self.backdropView.alpha = 0.0;
+                                       self.backdropView.image = smallImage;
+                                       
+                                       [UIView animateWithDuration:0.3
+                                                        animations:^{
+                                                            
+                                                            self.backdropView.alpha = 1.0;
+                                                            
+                                                        } completion:^(BOOL finished) {
+                                                            // The AFNetworking ImageView Category only allows one request to be sent at a time
+                                                            // per ImageView. This code must be in the completion block.
+                                                            [self.backdropView setImageWithURLRequest:backdropRequest
+                                                                                      placeholderImage:smallImage
+                                                                                               success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage * largeImage) {
+                                                                                                   self.backdropView.image = largeImage;
+                                                                                               }
+                                                                                               failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+                                                                                                   // do something for the failure condition of the large image request
+                                                                                                   // possibly setting the ImageView's image to a default image
+                                                                                               }];
+                                                        }];
+                                   }
+                                   failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+                                       // do something for the failure condition
+                                       // possibly try to get the large image
+                                   }];
+    
     
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
