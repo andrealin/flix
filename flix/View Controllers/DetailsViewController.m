bb6b79a8a0c30c545a53265f2811b8b427330ace
Andrea Lin <drealin@fb.com>
2019-06-28 14:55:59 -0700

    all images fade in as they are loading

--- flix/View Controllers/DetailsViewController.m
+++ flix/View Controllers/DetailsViewController.m
@@ -29,12 +29,66 @@
     NSString *posterURLString = self.movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
-    [self.posterView setImageWithURL:posterURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:posterURL];
+    
+    //    cell.posterView.image = nil;
+    //    [cell.posterView setImageWithURL:posterURL];
+    
+    [self.posterView setImageWithURLRequest:request placeholderImage:nil
+                                    success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
+                                        
+                                        // imageResponse will be nil if the image is cached
+                                        if (imageResponse) {
+                                            NSLog(@"Image was NOT cached, fade in image");
+                                            self.posterView.alpha = 0.0;
+                                            self.posterView.image = image;
+                                            
+                                            //Animate UIImageView back to alpha 1 over 0.3sec
+                                            [UIView animateWithDuration:0.3 animations:^{
+                                                self.posterView.alpha = 1.0;
+                                            }];
+                                        }
+                                        else {
+                                            NSLog(@"Image was cached so just update the image");
+                                            self.posterView.image = image;
+                                        }
+                                    }
+                                    failure:^(NSURLRequest *request, NSHTTPURLResponse * response, NSError *error) {
+                                        // do something for the failure condition
+                                    }];
     
     NSString *backdropURLString = self.movie[@"backdrop_path"];
     NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
     NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
-    [self.backdropView setImageWithURL:backdropURL];
+//    [self.backdropView setImageWithURL:backdropURL];
+//
+    NSURLRequest *backdropRequest = [NSURLRequest requestWithURL:backdropURL];
+    
+    //    cell.posterView.image = nil;
+    //    [cell.posterView setImageWithURL:posterURL];
+    
+    [self.backdropView setImageWithURLRequest:backdropRequest placeholderImage:nil
+                                    success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
+                                        
+                                        // imageResponse will be nil if the image is cached
+                                        if (imageResponse) {
+                                            NSLog(@"Backdrop Image was NOT cached, fade in image");
+                                            self.backdropView.alpha = 0.0;
+                                            self.backdropView.image = image;
+                                            
+                                            //Animate UIImageView back to alpha 1 over 0.3sec
+                                            [UIView animateWithDuration:0.3 animations:^{
+                                                self.backdropView.alpha = 1.0;
+                                            }];
+                                        }
+                                        else {
+                                            NSLog(@"Backdrop Image was cached so just update the image");
+                                            self.backdropView.image = image;
+                                        }
+                                    }
+                                    failure:^(NSURLRequest *request, NSHTTPURLResponse * response, NSError *error) {
+                                        // do something for the failure condition
+                                    }];
     
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
