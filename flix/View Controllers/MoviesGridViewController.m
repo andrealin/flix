9557dc5aa19f257778bc06e4c4512782e28ed103
Andrea Lin <drealin@fb.com>
2019-06-28 11:59:02 -0700

    collection view layout

--- flix/View Controllers/MoviesGridViewController.m
+++ flix/View Controllers/MoviesGridViewController.m
@@ -32,6 +32,17 @@
     // Do any additional setup after loading the view.
     [self.activityIndicator startAnimating];
     [self fetchMovies];
+    
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *)self.collectionView.collectionViewLayout;
+    
+    layout.minimumInteritemSpacing = 5;
+    layout.minimumLineSpacing = 5;
+    
+    CGFloat postersPerLine = 2;
+    CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumInteritemSpacing * (postersPerLine - 1)) / postersPerLine;
+    CGFloat itemHeight = itemWidth * 1.5;
+    
+    layout.itemSize = CGSizeMake(itemWidth, itemHeight);
 
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
